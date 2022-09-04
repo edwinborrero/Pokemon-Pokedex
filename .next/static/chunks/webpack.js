296c1cf62511e8ab7a000a163577e3f9bcaa222d
Edwin X. Borrero <37230972+edwinborrero@users.noreply.github.com>
2022-09-04 14:56:51 -0400

    animation in sprites

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99d309a80f7a0f7b"; }
+/******/ 		__webpack_require__.h = function() { return "71aa15e379cd60dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
